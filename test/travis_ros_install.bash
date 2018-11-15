8ddf4e8a0de35420f31b76284ae2b40c6c45d510
Kaoru Akira <ubedog180416@gmail.com>
2018-11-15 16:06:57 +0900

    Add package file

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,7 +8,7 @@ pip install rospkg
 
 #ros install
 cd..
-git clone http://github.com/ubedog/ros_setup_scripts_Ubuntu14.04_server.git
+git clone https://github.com/ubedog/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
