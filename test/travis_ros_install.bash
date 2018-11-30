95517400d328fe7c7ec77e968476a98077c0359c
Kaoru Akira <ubedog180416@gmail.com>
2018-11-30 13:34:53 +0900

    Add test

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,14 +1,25 @@
 #!/bin/bash -xve
 
 #required packages
+<<<<<<< HEAD
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
+
+#ros install
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server
+=======
 pip install catkin_pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
 
 #ros install
-cd..
+cd ..
 git clone https://github.com/ubedog/ros_setup_scripts_Ubuntu14.04_server.git
+>>>>>>> 90787d43ca79531b39d727e19463fa0425eb4b83
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
